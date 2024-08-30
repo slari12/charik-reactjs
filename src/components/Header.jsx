814b0fbd748a198fd4d72ce33f54252b3f4e2025
clari <clari-v@gmail.com>
2024-08-30 17:44:25 +0800

    modify mobile nav state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import Logo from "../assets/logo-horiz-charik-1.webp";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Home, Pricing, About, Resources } from "../pages";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   const [isDropdownVisible, setDropdownVisible] = useState(false);
   const [isDropdownVisible2, setDropdownVisible2] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  // const closeNav = () => {};
 
   const handleMouseEnter = () => {
     setDropdownVisible(true);
@@ -91,53 +96,76 @@ function Header() {
                   )}
                 </div>
               </div>
-
               <label
-                class="relative z-40 cursor-pointer px-3 py-6 mobile-nav"
+                class="relative z-40 cursor-pointer  mobile-nav"
                 for="mobile-menu"
               >
                 <input class="peer hidden" type="checkbox" id="mobile-menu" />
-                <div class="relative z-50 block h-[1px] w-7 bg-black bg-transparent content-[''] before:absolute before:top-[-0.35rem] before:z-50 before:block before:h-full before:w-full before:bg-black before:transition-all before:duration-200 before:ease-out before:content-[''] after:absolute after:right-0 after:bottom-[-0.35rem] after:block after:h-full after:w-full after:bg-black after:transition-all after:duration-200 after:ease-out after:content-[''] peer-checked:bg-transparent before:peer-checked:top-0 before:peer-checked:w-full before:peer-checked:rotate-45 before:peer-checked:transform after:peer-checked:bottom-0 after:peer-checked:w-full after:peer-checked:-rotate-45 after:peer-checked:transform"></div>
-                <div class="fixed inset-0 z-40 hidden h-full w-full bg-black/50 backdrop-blur-sm peer-checked:block">
-                  &nbsp;
-                </div>
-                <div class="fixed top-0 right-0 z-40 h-full w-full translate-x-full overflow-y-auto overscroll-y-none transition duration-500 peer-checked:translate-x-0">
-                  <div class="float-right min-h-full w-[85%] bg-white px-6 pt-12 shadow-2xl block">
-                    <p className="mt-[30px] mb-[10px]">Charik</p>
-                    <ul className="ml-6">
-                      <li className="mb-[10px]">
-                        <Link to="/">Software</Link> <br />
-                      </li>
-                      <li className="mb-[10px]">
-                        <Link to="/pricing">Email Finder</Link> <br />
-                      </li>
-                      <li className="mb-[10px]">
-                        <Link to="/pricing">Company Search</Link> <br />
-                      </li>
-                    </ul>
-                    <p className="mb-[10px]">
-                      <Link to="/pricing">Pricing</Link>
-                    </p>
-                    <p className="mb-[10px]">
-                      <Link to="/about" className="mb-[10px]">
-                        About
-                      </Link>
-                    </p>
 
-                    <p className="mb-[10px]">Resources</p>
-                    <ul className="ml-6">
-                      <li className="mb-[10px]">
-                        <Link to="/pricing">Insights</Link> <br />
-                      </li>
-                      <li className="mb-[10px]">
-                        <Link to="/pricing">Knowledge Base</Link> <br />
-                      </li>
-                      <li className="mb-[10px]">
-                        <Link to="/pricing">SDF Tester</Link> <br />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+                <FontAwesomeIcon
+                  icon={faBars}
+                  style={{ padding: "0" }}
+                  size="xl"
+                  onClick={() => setIsNavOpen(true)}
+                />
+
+                {isNavOpen && (
+                  <>
+                    <div class="fixed inset-0 z-40 hidden h-full w-full bg-black/50 backdrop-blur-sm peer-checked:block">
+                      &nbsp;
+                    </div>
+                    <div class="fixed top-0 left-0 z-40 h-full w-full translate-x-full overflow-y-auto overscroll-y-none transition duration-500 peer-checked:translate-x-0">
+                      <div class="float-right min-h-full w-[85%] bg-white pl-8 pr-6 pt-12 shadow-2xl block">
+                        <FontAwesomeIcon
+                          icon={faXmark}
+                          style={{
+                            position: "absolute",
+                            top: "20px",
+                            right: "26px",
+                          }}
+                          size="xl"
+                        />
+                        <p className=" mb-[10px]">Charik</p>
+                        <ul className="ml-6">
+                          <li
+                            className="mb-[10px]"
+                            onClick={() => setIsNavOpen(false)}
+                          >
+                            <Link to="/">Software</Link>
+                            <br />
+                          </li>
+                          <li className="mb-[10px]">
+                            <Link to="/pricing">Email Finder</Link> <br />
+                          </li>
+                          <li className="mb-[10px]">
+                            <Link to="/pricing">Company Search</Link> <br />
+                          </li>
+                        </ul>
+                        <p className="mb-[10px]">
+                          <Link to="/pricing">Pricing</Link>
+                        </p>
+                        <p className="mb-[10px]">
+                          <Link to="/about" className="mb-[10px]">
+                            About
+                          </Link>
+                        </p>
+
+                        <p className="mb-[10px]">Resources</p>
+                        <ul className="ml-6">
+                          <li className="mb-[10px]">
+                            <Link to="/pricing">Insights</Link> <br />
+                          </li>
+                          <li className="mb-[10px]">
+                            <Link to="/pricing">Knowledge Base</Link> <br />
+                          </li>
+                          <li className="mb-[10px]">
+                            <Link to="/pricing">SDF Tester</Link> <br />
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </>
+                )}
               </label>
             </div>
           </div>
